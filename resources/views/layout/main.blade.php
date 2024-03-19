b1ebf7008cba64e689540d9a89b5a61483fa06d7
martinhl2 <martin@thehighlightsapp.com>
2024-03-19 09:26:57 -0400

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -1,7 +1,7 @@
 @extends('../layout/base')
 
 @section('body')
-    <body class="p-5 side-menu-open" style="background-color: #025091 !important>
+    <body class="p-5 side-menu-open" style="background-color: #025091 !important">
         @yield('content')
 
 
